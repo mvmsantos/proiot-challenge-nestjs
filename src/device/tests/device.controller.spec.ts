0484cffcccf3009c1fdadeb5905b48046bafead6
Marcos Santos <marcos.infosantos@gmail.com>
2023-02-27 20:10:26 -0300

    feat: Added service tests unitaries (incomplete)
    tests unitaries incomplete

--- src/device/tests/device.controller.spec.ts
+++ src/device/tests/device.controller.spec.ts
@@ -2,19 +2,47 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { DeviceController } from '../device.controller';
 import { DeviceService } from '../device.service';
 
+const DeviceList = [
+  new DeviceService({ moisture: '10%', temperature: '30°C', name: 'test' }),
+];
+
 describe('DeviceController', () => {
-  let controller: DeviceController;
+  let DevicesController: DeviceController;
+  let DevicesService: DeviceService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [DeviceController],
-      providers: [DeviceService],
+      providers: [
+        {
+          provide: DeviceService,
+          useValue: {
+            findAll: jest.fn().mockResolvedValue(DeviceList),
+            create: jest.fn(),
+            findOne: jest.fn(),
+            update: jest.fn(),
+            delete: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
-    controller = module.get<DeviceController>(DeviceController);
+    DevicesController = module.get<DeviceController>(DeviceController);
+    DevicesService = module.get(DeviceService);
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(DevicesController).toBeDefined();
+    expect(DevicesService).toBeDefined();
+  });
+
+  describe('findAll', () => {
+    it('should return a devices list sucessfully', async () => {
+      // Act
+      const result = await DevicesController.findAll();
+
+      // Assert
+      expect(result).toEqual([DevicesService]);
+    });
   });
 });
